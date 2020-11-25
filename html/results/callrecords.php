a392708a4cbbdd43b0bcff5f48de455fc4a18fa6
Curtis Brazzell <curtbraz@gmail.com>
2020-11-24 23:56:27 -0500

    Create callrecords.php

---
+++ html/results/callrecords.php
@@ -0,0 +1,36 @@
+<?php
+
+$project = $_REQUEST['project'];
+$lastseen = $_REQUEST['lastseen'];
+
+// Read Database Connection Variables
+require_once '../../config.txt';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CALL GetAjaxRecords('$project','$lastseen');";
+$result = $conn->query($sql);
+
+//echo $sql;
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+//$pw = $row["pass"];
+//echo $row["username"]."\r\n".$row["password"]."\r\n";
+if($row != null){
+echo json_encode($row);
+}
+	}
+
+
+	
+//printf($conn->error);
+
+$conn->close();	
+
+?>
