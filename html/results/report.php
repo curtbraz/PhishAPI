7671d9f6d0eb0648058ec0e576dd008bf4ec9e49
Curtis Brazzell <curtbraz@gmail.com>
2020-11-26 00:41:40 -0500

    Update report.php

--- html/results/report.php
+++ html/results/report.php
@@ -28,7 +28,9 @@
 <?php
 
 // Read Database Connection Variables
-require_once '../../config.txt';
+ob_start();
+require '../config.php';
+ob_end_clean();
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
