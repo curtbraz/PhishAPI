bed3888dcc5b7871f9f348348af8cd54860d267b
root <root@ip-172-31-37-189.us-west-2.compute.internal>
2020-11-28 08:11:08 +0000

    Perms Issues Fixed

--- html/push/MyLogger.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
-
-function createLogger() : LoggerInterface
-{
-    // just create and configure any PSR-3 logger of your choice 
-    return new NullLogger();
-}
